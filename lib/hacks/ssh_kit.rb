08c6efb7af66dfe571a9195dc8b3c139e5a7fd99
Patrick Blesi <patrick.blesi@getbraintree.com>
2019-05-17 02:46:00 +0000

    Add full copyright notice and license for sshkit patch

--- lib/hacks/ssh_kit.rb
+++ lib/hacks/ssh_kit.rb
@@ -1,4 +1,30 @@
 ::SSHKit::Backend::Abstract.class_eval do
+  # Code taken from https://github.com/capistrano/sshkit/blob/v1.16.0/lib/sshkit/backends/abstract.rb#L98-L116
+  # Copyright (c) 2008- Lee Hambley & Contributors
+  # License link: https://github.com/capistrano/sshkit/blob/v1.16.0/LICENSE.md
+  #
+  # Full copyright notice and license:
+  #
+  # Copyright (c) 2008- Lee Hambley & Contributors
+  #
+  # Permission is hereby granted, free of charge, to any person obtaining a
+  # copy of this software and associated documentation files (the "Software"),
+  # to deal in the Software without restriction, including without limitation
+  # the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  # and/or sell copies of the Software, and to permit persons to whom the
+  # Software is furnished to do so, subject to the following conditions:
+  #
+  # The above copyright notice and this permission notice shall be included
+  # in all copies or substantial portions of the Software.
+  #
+  # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  # DEALINGS IN THE SOFTWARE.
+
   def as(who, &_block)
     if who.is_a? Hash
       @user  = who[:user]  || who["user"]
