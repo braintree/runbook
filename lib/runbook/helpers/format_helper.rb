f88c71cf36c129a46af3e343fd80e4d7e43d15fe
Patrick Blesi <patrick.blesi@getbraintree.com>
2019-06-09 17:27:51 +0000

    Fix format_helper indentation bug

--- lib/runbook/helpers/format_helper.rb
+++ lib/runbook/helpers/format_helper.rb
@@ -2,8 +2,9 @@ module Runbook::Helpers
   module FormatHelper
     def deindent(str, padding: 0)
       lines = str.split("\n")
-      indentation = lines[0].size - lines[0].gsub(/^\s+/, "").size
-      lines.map! { |line| " " * padding + line[indentation..-1] }
+      indentations = lines.map { |l| l.size - l.gsub(/^\s+/, "").size }
+      min_indentation = indentations.min
+      lines.map! { |line| " " * padding + line[min_indentation..-1] }
       lines.join("\n")
     end
   end
--- spec/helpers/format_helper_spec.rb
+++ spec/helpers/format_helper_spec.rb
@@ -21,6 +21,44 @@ STR
       expect(subject.deindent(str)).to eq(result.strip)
     end
 
+    context "when the first line is not the least indented" do
+      let(:str) do <<-STR
+          This message
+        has some
+          leading whitespace
+                      STR
+      end
+      let(:result) do <<-STR
+    This message
+  has some
+    leading whitespace
+      STR
+      end
+
+      it "adds padding in front of each line" do
+        expect(subject.deindent(str, padding: 2)).to eq(result.rstrip)
+      end
+
+      context "when the second line has no indentation" do
+        let(:str) do <<-STR
+            This message
+has some
+            leading whitespace
+                        STR
+        end
+        let(:result) do <<-STR
+              This message
+  has some
+              leading whitespace
+        STR
+        end
+
+        it "adds padding in front of each line" do
+          expect(subject.deindent(str, padding: 2)).to eq(result.rstrip)
+        end
+      end
+    end
+
     context "when padding is specified" do
       let(:str) do <<-STR
           This message
